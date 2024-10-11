0756e4a7f973cb47830e94095ce88e6900661903
nurhan004 <sharshenbekov777@gmail.com>
2024-10-11 18:26:21 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+// console.log('hello')
+// console.log('nurxan')
+// console.error('error')
+// console.info('info')
+// // console.warn('warn')
+// alert('hello')
+//  prompt('How old are you?')
+
+// переменные
+// ES5 var,ES6 let const
+
+
+// var age = 100
+// var age = 10
+// console.log (age);
+
+
+// // let name = 'nurhan'
+// let name = 'baiel'
+// name = 'baiel'
+
+
+
+
+// console.log(name)
+
+
+
+// // const animal = 'lion'
+// animal = 'tiger'
+// console.log(animal);
+
+
+// const name = 'Nurhan'
+// const hobbi = 'play to footbol'
+// const old = '16'
+
+
+// console.log(`менин атым  ${name} менин хоббим ${hobbi} менин жашым ${old}`)
+
+
+const name = prompt('сенин атын ким?')
+const age = prompt('сен канча жаштасын?')
+const hobby = prompt('сен неге кызыхысын?')
+
+console.log(name);
+console.log(age);
+console.log(hobby);
+
+
+
+
+alert(`сенин атын ${name} сен канча жаштасын ${age} сен неге кызыгасын ${hobby}`)
+
+
+
